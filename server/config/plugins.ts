8b46e6016f2992b60dfe5980f22c7d27bd88ab9f
Ito Taichi <67674781+t4ichi@users.noreply.github.com>
2025-04-13 16:43:52 +0900

    feat: 画像をアップロードしたときにwebpに変換する機能を追加

--- server/config/plugins.ts
+++ server/config/plugins.ts
@@ -1,1 +1,12 @@
-export default () => ({});
+export default () => ({
+  "webp-converter": {
+    enabled: true,
+    config: {
+      // mimeTypes that converts to WebP. Default is ['image/png', 'image/jpeg', 'image/jpg']
+      mimeTypes: ["image/png", "image/jpeg", "image/jpg"],
+      options: {
+        // WebP options: https://sharp.pixelplumbing.com/api-output#webp
+      },
+    },
+  },
+});
