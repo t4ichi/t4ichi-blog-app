e845e3aa7450da9943c0e072af4d58d608594846
Ito Taichi <67674781+t4ichi@users.noreply.github.com>
2025-06-01 13:08:35 +0900

    feat: park-uiを追加

--- panda.config.ts
+++ panda.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from "@pandacss/dev";
+import { createPreset } from "@park-ui/panda-preset";
+import amber from "@park-ui/panda-preset/colors/amber";
+import sand from "@park-ui/panda-preset/colors/sand";
 
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
 
+  presets: [
+    createPreset({ accentColor: amber, grayColor: sand, radius: "sm" }),
+  ],
+
   // Where to look for your css declarations
   include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+
+  jsxFramework: "react",
 
   // Files to exclude
   exclude: [],
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { HStack } from "styled-system/jsx";
 import { css } from "../../styled-system/css";
 
 export default function Home() {
   return (
-    <div className={css({ fontSize: "2xl", fontWeight: "bold" })}>
-      Hello 🐼!
-    </div>
+    <>
+      <div className={css({ fontSize: "2xl", fontWeight: "bold" })}>
+        Hello 🐼!
+      </div>
+      <HStack>
+        <Button colorPalette="red" variant="solid">
+          Button
+        </Button>
+        <Button colorPalette="red" variant="subtle">
+          Button
+        </Button>
+        <Button colorPalette="red" variant="outline">
+          Button
+        </Button>
+        <Button colorPalette="red" variant="ghost">
+          Button
+        </Button>
+      </HStack>
+    </>
   );
 }
