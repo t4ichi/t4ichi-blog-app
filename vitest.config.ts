c580bd1e8f4c7e011865f7493c99120fb1ae4402
Ito Taichi <67674781+t4ichi@users.noreply.github.com>
2025-06-14 23:45:44 +0900

    feat: storybookを追加

---
+++ vitest.config.ts
@@ -0,0 +1,48 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+const dirname =
+  typeof __dirname !== "undefined"
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+export default defineConfig({
+  test: {
+    workspace: [
+      // ユニットテスト
+      // {
+      //   plugins: [tsconfigPaths(), react()],
+      //   test: {
+      //     name: "unit",
+      //     include: ["src/**/*.test.@(ts|tsx)"],
+      //     environment: "jsdom",
+      //   },
+      // },
+      // Storybook
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({ configDir: path.join(dirname, ".storybook") }),
+        ],
+
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: "playwright",
+            instances: [{ browser: "chromium" }],
+          },
+          setupFiles: [".storybook/vitest.setup.ts"],
+        },
+      },
+    ],
+  },
+});
