97cc4d9fad49e066c55b2d4e459e9b57f81ae8c3
Ito Taichi <67674781+t4ichi@users.noreply.github.com>
2025-06-14 14:03:38 +0900

    feat: svgrを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import SteamingBowlColor from "../../public/steaming_bowl_color.svg";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <SteamingBowlColor className="w-20 h-20" />
+    </>
+  );
 }
